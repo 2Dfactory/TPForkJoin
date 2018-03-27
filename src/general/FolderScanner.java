de5968c05e9d3f1177a2d0b56e350943f9c69062
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-03-27 16:01:57 +0200

    Ajout des méthodes pour le fork/join

--- src/general/FolderScanner.java
+++ src/general/FolderScanner.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
 
-public class FolderScanner {
+public class FolderScanner extends RecursiveTask<Long>{
 
 	private Path path = null;
 	private String filter = "*";
@@ -52,9 +55,68 @@ public class FolderScanner {
 		return result;
 		
 	}
+
+	public Long parallelScan() throws ScanException {
+		
+		List<FolderScanner> list = new ArrayList<>();
+		
+		if(path == null || path.equals("")) throw new ScanException("Chemin à scanner non valide (vide ou null) !");
+		
+		System.out.println("Scan du dossier : " + path + " à la recherche des fichiers portant l'extension " + filter);
+		
+		try(DirectoryStream<Path> listing = Files.newDirectoryStream(path)){
+			
+			for(Path nom : listing) {
+				if(Files.isDirectory(nom.toAbsolutePath())) {
+					FolderScanner f = new FolderScanner(nom.toAbsolutePath(), filter);
+					list.add(f);
+					f.fork();
+				}
+			}
+		
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		try(DirectoryStream<Path> listing = Files.newDirectoryStream(path, filter)){
+			
+			for(Path nom : listing) {
+				result++;
+			}
+			
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();			
+		}
+		
+		for(FolderScanner f : list) result += f.join();
+		
+		return result;
+	}
 	
 	
 	
 	
+	@Override
+	protected Long compute() {
+		
+		long resultat = 0;
+		
+		try {
+			resultat = this.parallelScan();
+		}
+		catch(ScanException se) {
+			se.printStackTrace();
+		}
+
+		return resultat;
+		
+	}
+	
+	
+	public long getResultat() {
+		return this.result;
+	}
 	
 }
--- src/general/Main.java
+++ src/general/Main.java
@@ -2,6 +2,7 @@ package general;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
@@ -11,21 +12,22 @@ public class Main {
 		String filtre = "*.psd";
 		FolderScanner fs = new FolderScanner(chemin, filtre);
 		
-		try {
-			
-			Long start = System.currentTimeMillis();
-			Long resultat = fs.sequentialScan();
-			Long end = System.currentTimeMillis();
-			
-			System.out.println("Il y a " + resultat + "fichier(s) portant l'extension " + filtre);
-			System.out.println("Temps de traitement : " + (end-start) + "ms.");
-						
-			
-		}
-		catch(ScanException se) {
-			se.printStackTrace();
-		}
+		int processeurs = Runtime.getRuntime().availableProcessors();
 		
+		ForkJoinPool pool = new ForkJoinPool(processeurs);
+
+		Long start = System.currentTimeMillis();
+		
+		pool.invoke(fs);
+		
+		
+		//Long resultat = fs.sequentialScan();
+		Long end = System.currentTimeMillis();
+		
+		System.out.println("Il y a " + fs.getResultat() + "fichier(s) portant l'extension " + filtre);
+		System.out.println("Temps de traitement : " + (end-start) + "ms.");
+						
+		System.out.println("Nbre de processeurs : " + processeurs);	
 		
 		
 		
