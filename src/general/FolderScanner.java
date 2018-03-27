13cf20c5fb5ada5b85d2da91cba2246052b804b0
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-03-27 15:35:45 +0200

    Création des classes

---
+++ src/general/FolderScanner.java
@@ -0,0 +1,60 @@
+package general;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FolderScanner {
+
+	private Path path = null;
+	private String filter = "*";
+	private long result = 0;
+	
+	public FolderScanner() {	}
+	
+	public FolderScanner(Path p, String f) {
+		path = p;
+		filter = f;
+	}
+	
+	public long sequentialScan() throws ScanException{
+		
+		if(path == null || path.equals("")) throw new ScanException("Chemin à scanner non valide (vide ou null) !");
+		
+		System.out.println("Scan du dossier : " + path + " à la recherche des fichiers portant l'extension " + filter);
+		
+		try(DirectoryStream<Path> listing = Files.newDirectoryStream(path)){
+			
+			for(Path nom : listing) {
+				if(Files.isDirectory(nom.toAbsolutePath())) {
+					FolderScanner f = new FolderScanner(nom.toAbsolutePath(), filter);
+					result += f.sequentialScan();
+				}
+			}
+		
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		try(DirectoryStream<Path> listing = Files.newDirectoryStream(path, filter)){
+			
+			for(Path nom : listing) {
+				result++;
+			}
+			
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();			
+		}
+		
+		return result;
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/general/Main.java
@@ -0,0 +1,35 @@
+package general;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Path chemin = Paths.get("C:\\Users\\vzlskl\\Documents");
+		String filtre = "*.psd";
+		FolderScanner fs = new FolderScanner(chemin, filtre);
+		
+		try {
+			
+			Long start = System.currentTimeMillis();
+			Long resultat = fs.sequentialScan();
+			Long end = System.currentTimeMillis();
+			
+			System.out.println("Il y a " + resultat + "fichier(s) portant l'extension " + filtre);
+			System.out.println("Temps de traitement : " + (end-start) + "ms.");
+						
+			
+		}
+		catch(ScanException se) {
+			se.printStackTrace();
+		}
+		
+		
+		
+		
+		
+	}
+
+}
